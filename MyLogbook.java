dde213934619ca2055b9459c5a4cf85689bb76f6
Julita K <julitakuczkowska@gmail.com>
2021-07-28 01:30:39 +0200

    VOL_1 create MyLogbook class and create print method inside them

---
+++ MyLogbook.java
@@ -0,0 +1,10 @@
+public class MyLogbook {
+	public static void main(String args[]) {
+		System.out.println("My Logbook application\n" +
+							"foreach loop -> read allDives ArrayList\n" +
+							"1, Temple, Egypt, 55, 19, Bartek, air\n" +
+							"2, Thistlegorm, Egypt, 38, 26, Pejsebela, air\n" +
+							"3, Rury kopalni Wapienniki, Poland, 59, 6, Bartek, nitrox\n" +
+							"4, Um el Faroud, Malta,33, 45, Grzesiek, air");
+	}
+}
