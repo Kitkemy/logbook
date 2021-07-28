638116b825401eec8fb4545c62bc3cd03ca87aed
Julita K <julitakuczkowska@gmail.com>
2021-07-28 02:34:43 +0200

    VOL_4 move static class DIVE outside MyLogbook class (change on non static class

--- MyLogbook.java
+++ MyLogbook.java
@@ -18,28 +18,28 @@ public class MyLogbook {
 		System.out.println(dive3);
 		System.out.println(dive4);
 	}
+}
+
+class Dive {
+	int numberDive;
+	String place;
+	String country;
+	int diveTime;
+	int maxDeep;
+	String buddy;
+	String gas;
 	
-	static class Dive {
-		int numberDive;
-		String place;
-		String country;
-		int diveTime;
-		int maxDeep;
-		String buddy;
-		String gas;
-		
-		Dive(int numberDive, String place, String country, int diveTime, int maxDeep, String buddy, String gas) {
-			this.numberDive = numberDive;
-			this.place = place;
-			this.country = country;
-			this.diveTime = diveTime;
-			this.maxDeep = maxDeep;
-			this.buddy = buddy;
-			this.gas = gas;
-		}
-		
-		public String toString() {
-			return numberDive + ", " + place + ", " + country + ", " + diveTime + ", " + maxDeep + ", " + buddy + ", " + gas;
-		}
+	Dive(int numberDive, String place, String country, int diveTime, int maxDeep, String buddy, String gas) {
+		this.numberDive = numberDive;
+		this.place = place;
+		this.country = country;
+		this.diveTime = diveTime;
+		this.maxDeep = maxDeep;
+		this.buddy = buddy;
+		this.gas = gas;
+	}
+	
+	public String toString() {
+		return numberDive + ", " + place + ", " + country + ", " + diveTime + ", " + maxDeep + ", " + buddy + ", " + gas;
 	}
 }
