0dcae2dd0cc81e06c1fd5e1582bf15f264a73a72
Julita K <julitakuczkowska@gmail.com>
2021-07-28 14:05:15 +0200

    VOL_7 add two methods to MyLogbook class

--- MyLogbook.java
+++ MyLogbook.java
@@ -1,20 +1,23 @@
 import java.util.ArrayList;
 
 public class MyLogbook {
+	ArrayList<Dive> allDives = new ArrayList<>();
+	
 	MyLogbook() {
 		System.out.println("My Logbook application\n" +
-							"foreach loop -> read allDives ArrayList\n" +
-							"1, Temple, Egypt, 55, 19, Bartek, air\n" +
-							"2, Thistlegorm, Egypt, 38, 26, Pejsebela, air\n" +
-							"3, Rury kopalni Wapienniki, Poland, 59, 6, Bartek, nitrox\n" +
-							"4, Um el Faroud, Malta, 45, 33, Grzesiek, air\n");
-		
-		ArrayList<Dive> allDives = new ArrayList<>();
+							"foreach loop -> read allDives ArrayList\n");
+		addFirstArrayList(allDives);
+		readArrayList(allDives);
+	}
+	
+	public void addFirstArrayList(ArrayList<Dive> dive) {
 		allDives.add(new Dive(1, "Temple", "Egypt", 55, 19, "Bartek", "air"));
 		allDives.add(new Dive(2, "Thistlegorm", "Egypt", 38, 26, "Pejsebela", "air"));
 		allDives.add(new Dive(3, "Rury kopalni Wapienniki", "Poland", 59, 6, "Bartek", "nitrox"));
-		allDives.add(new Dive(4, "Um el Faroud", "Malta", 45, 33, "Grzesiek", "air"));		
+		allDives.add(new Dive(4, "Um el Faroud", "Malta", 45, 33, "Grzesiek", "air"));
+	}
 	
+	public void readArrayList(ArrayList<Dive> allDives) {
 		System.out.println();
 		for(Dive dive : allDives) {
 			System.out.println(dive);
